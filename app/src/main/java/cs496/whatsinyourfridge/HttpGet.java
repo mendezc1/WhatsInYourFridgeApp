1f22a234e2b341f39a15a8008e4f4f66ebb41914
Chris Mendez <mendezc@oregonstate.edu>
2016-05-03 18:05:05 -0700

    Conecting to internet

---
+++ app/src/main/java/cs496/whatsinyourfridge/HttpGet.java
@@ -0,0 +1,65 @@
+package cs496.whatsinyourfridge;
+
+import android.util.Log;
+
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.HashMap;
+import java.util.Map;
+
+public class HttpGet {
+
+    private Map<String, String> params = new HashMap<String, String>();
+    private String charset;
+    private String surl;
+
+    public HttpGet(String url) {
+        this.surl = url;
+        this.charset = "UTF-8";
+    }
+
+    public void addFormField(String name, String value) {
+        params.put(name, value);
+    }
+
+    public String finish() throws Exception {
+        // adapted from HttpPost
+
+        StringBuilder queryString = new StringBuilder();
+        for (String key : params.keySet()) {
+            if (queryString.length() != 0)
+                queryString.append('&');
+            else
+                queryString.append('?');
+            queryString.append(URLEncoder.encode(key, charset));
+            queryString.append('=');
+            queryString.append(URLEncoder.encode(params.get(key), charset));
+        }
+
+        URL url = new URL(surl + queryString);
+
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setRequestMethod("GET");
+
+        Reader in = new BufferedReader(new InputStreamReader(
+                conn.getInputStream(), charset));
+        StringBuffer rv = new StringBuffer();
+        for (int c; (c = in.read()) >= 0; rv.append((char) c));
+//        rv.toString();
+        Object obj = rv;
+        JSONObject jsonObject = (JSONObject) obj;
+        String title = (String) jsonObject.get("title");
+        String source_url = (String) jsonObject.get("source_url");
+        //JSONArray companyList = (JSONArray) jsonObject.get("Company List");
+        Log.d("Title ", title);
+        Log.d("Sourceurl", source_url);
+        return rv.toString();
+    }
+
+}
--- app/src/main/java/cs496/whatsinyourfridge/WhatsInYourFridge.java
+++ app/src/main/java/cs496/whatsinyourfridge/WhatsInYourFridge.java
@@ -4,15 +4,22 @@ import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.ServiceConnection;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.IBinder;
+import android.os.Looper;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.method.ScrollingMovementMethod;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
+
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
 
 import static cs496.whatsinyourfridge.R.id.edit_message;
 
@@ -26,11 +33,34 @@ public class WhatsInYourFridge extends AppCompatActivity implements ServiceConne
         setSupportActionBar(toolbar);
 
         // make sure the service is running
-        Context app = getApplicationContext();
-        Intent intent = new Intent(app, SearchService.class);
-        app.startService(intent);
+        //Context app = getApplicationContext();
+       // Intent intent = new Intent(app, SearchService.class);
+        //app.startService(intent);
+        View v = findViewById(R.id.mainLayoutID);
+        doGet(v);
     }
+    public void doGet(View v) {
+        new AsyncTask<Void, Void, String>() {
+            protected void onPreExecute() {
+                setText("Please wait...");
+            }
 
+            protected String doInBackground(Void... params) {
+                HttpGet http = new HttpGet("http://food2fork.com/api/search?key=8fb888939f3d819b54a8c4f41cf9822f&q=shredded%20chicken");
+                //http.addFormField("university", "Oregon State University");
+                //http.addFormField("course", "mobile and cloud development");
+                try {
+                    return http.finish();
+                } catch (Exception e) {
+                    return formatError(e);
+                }
+            }
+
+            protected void onPostExecute(String txt) {
+                setText(txt);
+            }
+        }.execute();
+    }
     private SearchService service;
     @Override
     protected void onStart() {
@@ -89,6 +119,37 @@ public class WhatsInYourFridge extends AppCompatActivity implements ServiceConne
         int duration = Toast.LENGTH_SHORT;
         Toast toast = Toast.makeText(context, "Pizza!", duration);
         toast.show();
+    }
+
+    private void setText(final String str) {
+        if (Looper.getMainLooper() == Looper.myLooper()) {
+            TextView txtResult = (TextView) (findViewById(R.id.edit_message));
+            if (txtResult != null) {
+               // Context context = getApplicationContext();
+               // int duration = Toast.LENGTH_SHORT;
+               // Toast toast = Toast.makeText(context, "setting text", duration);
+               // toast.show();
+                txtResult.setText(str);
+                txtResult.setMovementMethod(new ScrollingMovementMethod());
+                txtResult.scrollTo(0, 0);
+            }
+        } else {
+            runOnUiThread(
+                    new Runnable() {
+                        @Override
+                        public void run() {
+                            setText(str);
+                        }
+                    }
+            );
+        }
+    }
+
+    private String formatError(Exception ex) {
+        StringWriter tmp = new StringWriter();
+        tmp.append("An exception has occurred...\n");
+        ex.printStackTrace(new PrintWriter(tmp));
+        return tmp.toString();
     }
 }
 
